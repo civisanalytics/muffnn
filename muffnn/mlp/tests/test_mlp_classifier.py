73ab180b2712c4a5f07ae1941faff0190770106e
Sam Weiss <sweiss@civisanalytics.com>
2017-01-30 13:04:35 -0600

    adding tests for multilabel with NAs

--- muffnn/mlp/tests/test_mlp_classifier.py
+++ muffnn/mlp/tests/test_mlp_classifier.py
@@ -32,6 +32,9 @@ Y2 = [2, 1, 0]
 Y_multilabel = np.array([[0, 1, 0],
                          [1, 1, 0],
                          [1, 0, 1]])
+Y_multilabel_na = np.array([[0, 1, 0],
+                         [-1, 1, -1],
+                         [1, 0, 1]])
 
 # The defaults kwargs don't work for tiny datasets like those in these tests.
 KWARGS = {"random_state": 0, "n_epochs": 100, "batch_size": 1}
@@ -65,11 +68,31 @@ def check_multilabel_predictions(clf, X, y):
     assert np.sum(np.abs(probabilities.sum(axis=1) - 1)) > 0.01,\
         "multilabel classifier is outputting distributions"
 
+def check_multilabel_predictions_na(clf, X, y):
+    predicted = clf.fit(X, y).predict(X)
+    #change nans to zero to 'mask' those values
+    is_na=(y==-1)
+    predicted[is_na]=0
+    y[is_na]=0
+
+    assert_equal(predicted.shape, y.shape)
+    predicted[is_na]=0
+    y[is_na]=0
+    assert_array_equal(predicted, y)
+
+    probabilities = clf.predict_proba(X)
+    assert_equal(probabilities.shape, y.shape)
+    assert_array_equal((probabilities >= 0.5).astype(np.int), predicted)
+    assert np.sum(np.abs(probabilities.sum(axis=1) - 1)) > 0.01,\
+        "multilabel classifier is outputting distributions"
 
 def test_multilabel():
     check_multilabel_predictions(MLPClassifier(**KWARGS), X, Y_multilabel)
     check_multilabel_predictions(MLPClassifier(**KWARGS), X_sp, Y_multilabel)
 
+def test_multilabel_na():
+    check_multilabel_predictions_na(MLPClassifier(**KWARGS), X, Y_multilabel_na)
+    check_multilabel_predictions(MLPClassifier(**KWARGS), X_sp, Y_multilabel_na)
 
 def test_predict_2_classes():
     """Test binary classification."""
