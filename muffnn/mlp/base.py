5c3c4604999695bef15b4235400de0a7b270a6bb
Stephen Hoover <shoover@civisanalytics.com>
2019-06-21 14:17:36 -0500

    STY Fix method indentation
    Pass `flake8`; the method body was over-indented.

--- muffnn/mlp/base.py
+++ muffnn/mlp/base.py
@@ -446,30 +446,30 @@ class MLPBaseEstimator(TFPicklingBase, BaseEstimator):
         return embedding
 
     def fit_transform(self, X, y=None, **fit_params):
-            """Fit to data, then transform it.
+        """Fit to data, then transform it.
 
-            Fits transformer to X and y with optional parameters fit_params
-            and returns a transformed version of X.
+        Fits transformer to X and y with optional parameters fit_params
+        and returns a transformed version of X.
 
-            Parameters
-            ----------
-            X : numpy array of shape [n_samples, n_features]
-                Training set.
-            y : numpy array of shape [n_samples]
-                Target values.
-            Returns
-            -------
-            X_new : numpy array of shape [n_samples, n_features_new]
-                Transformed array.
-            """
-            # non-optimized default implementation; override when a better
-            # method is possible for a given clustering algorithm
-            if y is None:
-                # fit method of arity 1 (unsupervised transformation)
-                return self.fit(X, **fit_params).transform(X)
-            else:
-                # fit method of arity 2 (supervised transformation)
-                return self.fit(X, y, **fit_params).transform(X)
+        Parameters
+        ----------
+        X : numpy array of shape [n_samples, n_features]
+            Training set.
+        y : numpy array of shape [n_samples]
+            Target values.
+        Returns
+        -------
+        X_new : numpy array of shape [n_samples, n_features_new]
+            Transformed array.
+        """
+        # non-optimized default implementation; override when a better
+        # method is possible for a given clustering algorithm
+        if y is None:
+            # fit method of arity 1 (unsupervised transformation)
+            return self.fit(X, **fit_params).transform(X)
+        else:
+            # fit method of arity 2 (supervised transformation)
+            return self.fit(X, y, **fit_params).transform(X)
 
     def prediction_gradient(self, X):
         """Compute the prediction gradient with respect to the given inputs.
