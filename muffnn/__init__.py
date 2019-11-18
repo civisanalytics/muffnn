41f2635020e45d62d305c6f8c3e05cdd99139610
jacksonllee <jacksonlee@civisanalytics.com>
2019-11-18 14:12:14 -0600

    MAINT bump version to 2.3.0

--- muffnn/__init__.py
+++ muffnn/__init__.py
@@ -1,14 +1,19 @@
+import pkg_resources
+
 from .mlp.mlp_classifier import MLPClassifier
 from .mlp.mlp_regressor import MLPRegressor
 from .autoencoder.autoencoder import Autoencoder
 from .fm.fm_classifier import FMClassifier
 from .fm.fm_regressor import FMRegressor
 from .core import TFPicklingBase
-from .version import __version__  # noqa
+
+
+__version__ = pkg_resources.get_distribution('muffnn').version
 
 __all__ = ['MLPClassifier',
            'MLPRegressor',
            'Autoencoder',
            'FMClassifier',
            'FMRegressor',
-           'TFPicklingBase']
+           'TFPicklingBase',
+           '__version__']
