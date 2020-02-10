58004ba0e7387bec88a40286f1da103dee01b36d
jacksonllee <jacksonlee@civisanalytics.com>
2020-02-10 13:33:38 -0600

    DEP try scikit-learn<0.22.1

--- setup.py
+++ setup.py
@@ -28,6 +28,6 @@ setup(
     ],
     install_requires=['numpy>=1.17',
                       'scipy>=1.4',
-                      'scikit-learn>=0.19',
+                      'scikit-learn>=0.19,<0.22.1',
                       'tensorflow>=1.12.1,<2']
 )
